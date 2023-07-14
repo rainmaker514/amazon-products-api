cc6afac8fe47db48f81ccf27d78a16c1724a12f2
rainmaker514 <tdoug016@fiu.edu>
2023-07-14 15:59:29 -0400

    now getting all product titles on first page

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: false,
+        //headless: false,
         defaultViewport: false,
         userDataDir: "./tmp"
     });
     const page = await browser.newPage();
     await page.goto('https://www.amazon.com/gp/bestsellers/electronics/ref=zg_bs_electronics_sm');
+    const productHandles = await page.$$('.p13n-gridRow._cDEzb_grid-row_3Cywl > .a-column.a-span12.a-text-center._cDEzb_grid-column_2hIsc');
     
-    //await browser.close();
+    for(const productHandle of productHandles){
+        const title = await page.evaluate(el => el.querySelector("span > div").textContent, productHandle);
+        console.log(title);
+    }
+    
+    await browser.close();
 })();
