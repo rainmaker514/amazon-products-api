efcb8d552d5a83342e4791bfbaad5b5b995884ef
rainmaker514 <tdoug016@fiu.edu>
2023-07-27 11:27:58 -0400

    now getting all product prices and images on first page

--- index.js
+++ index.js
@@ -11,8 +11,23 @@ const puppeteer = require('puppeteer');
     const productHandles = await page.$$('.p13n-gridRow._cDEzb_grid-row_3Cywl > .a-column.a-span12.a-text-center._cDEzb_grid-column_2hIsc');
     
     for(const productHandle of productHandles){
-        const title = await page.evaluate(el => el.querySelector("span > div").textContent, productHandle);
-        console.log(title);
+        let title = "null";
+        let price = "null";
+        let img = "null";
+
+        try {
+            title = await page.evaluate(el => el.querySelector("span > div").textContent, productHandle);
+        } catch (error) {}
+
+        try {
+            price = await page.evaluate(el => el.querySelector("span > span").textContent, productHandle);
+        } catch (error) {}
+        
+        try {
+            img = await page.evaluate(el => el.querySelector(".a-dynamic-image.p13n-sc-dynamic-image.p13n-product-image").getAttribute("src"), productHandle);
+        } catch (error) {}
+        
+        console.log(title, price, img);
     }
     
     await browser.close();
