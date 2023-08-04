f340269a313f3ed75d3d4bbb07bd6e55055fb187
rainmaker514 <tdoug016@fiu.edu>
2023-08-04 10:20:02 -0400

    added cluster version of original code, scrapes multiple pages at the same time, ready for deployment

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const {Cluster} = require('puppeteer-cluster');
 
 (async () => {
     const browser = await puppeteer.launch({
-        //headless: false,
-        //defaultViewport: false,
         userDataDir: "./tmp"
     });
     const page = await browser.newPage();
@@ -14,12 +13,16 @@ const fs = require('fs');
         height: 800
     });
     
-    //let items = [];
     let isButtonDisabled = false;
-    while(!isButtonDisabled){
-        await autoScroll(page);
+    //creating csv
+    fs.writeFile('results.csv', 'title,price,image', err => {
+        if(err) throw err;
+    });
+    while(!isButtonDisabled){ //checking if 'next' button is disabled, if it is, job is done
+        await autoScroll(page);//function that scrolls through page to load all items
         const productHandles = await page.$$('.p13n-gridRow._cDEzb_grid-row_3Cywl > .a-column.a-span12.a-text-center._cDEzb_grid-column_2hIsc');
         
+        //gets each product and adds info to file with each iteration, if a field is missing it will automatically be 'null' 
         for(const productHandle of productHandles){ 
             let title = "null";
             let price = "null";
@@ -37,23 +40,20 @@ const fs = require('fs');
                 img = await page.evaluate(el => el.querySelector(".a-dynamic-image.p13n-sc-dynamic-image.p13n-product-image").getAttribute("src"), productHandle);
             } catch (error) {}
             
-            //items.push({title, price, img});
-
-            fs.appendFile('results.csv', `${title.replace(/,/g, ".")},${price},${img}\n`, function(err){
-            if(err) throw err;
-    });
+            fs.appendFile('results.csv', `\n${title.replace(/,/g, ".")},${price},${img}`, err => {
+                if(err) throw err;
+            });
         }
         
+        //before while loop restarts, check for 'next' button disable, if not, click it and restart the loop
         isButtonDisabled = await page.$(".a-disabled.a-last") !== null;
         
         if(!isButtonDisabled){
             await page.click(".a-last");
             await new Promise(r => setTimeout(r, 2000));
         }
-}
+    }
     
-    // console.log(items);    
-    // console.log(items.length);    
     await browser.close();
 })();
 
