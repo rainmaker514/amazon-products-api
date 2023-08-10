500ff3d09d9dd220790b29748fd6f62dc6918968
rainmaker514 <tdoug016@fiu.edu>
2023-08-10 00:55:00 -0400

    update

--- indexv2.js
+++ indexv2.js
@@ -108,7 +108,7 @@ async function getProducts(link){
             '--single-process',
             '--no-zygote'
         ],
-        executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH: puppeteer.executablePath()
+        executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH: puppeteer.executablePath()
     });
 
     const page = await browser.newPage();
