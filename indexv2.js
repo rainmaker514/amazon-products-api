8d3ea1d4bb70e80301761537f052a64117bc0d54
rainmaker514 <tdoug016@fiu.edu>
2023-08-24 12:06:26 -0400

    update

--- indexv2.js
+++ indexv2.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer-extra');
+const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const express = require('express');
 const app = express();
